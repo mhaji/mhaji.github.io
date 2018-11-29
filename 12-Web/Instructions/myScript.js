77ab88c81d72e50cf7fdf5028e54da0acd1f1f89
mhaji <minhajhaji2019@u.northwestern.edu>
2018-11-29 06:03:25 -0600

    first commit

---
+++ 12-Web/Instructions/myScript.js
@@ -0,0 +1,13 @@
+var home;
+var Data;
+var Comparision;
+
+var maxTemp;
+var humidity;
+var cloudiness;
+
+var windSpeed;
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+ }
